f5dae705b4350f780f91e11fdc3b57845e5d9425
Laureline Auger <laureline.auger@gmail.com>
2025-01-29 16:15:11 +0100

    ajout champ ACF logo technologies + affichage

--- single-realisation.php
+++ single-realisation.php
@@ -12,46 +12,58 @@
         // Récupération année (date de publication)
         $annee = get_the_date('Y');
     ?>
-    <div class="single-photo">
+    <div class="single-realisation">
         <figure id="post-<?php the_ID(); ?>" class="container"<?php post_class(); ?>>
             <div class="photo-infos">
                 <h1><?php the_title(); ?></h1>
                 <div class="photo-meta">
-                    <p><?php
-                        foreach ($categories as $categorie) {
-                            echo $categorie->name . ' '; 
-                        } ?>
-                    </p> 
-                    <p><?php echo $annee; ?></p>
-                    <p>Technologies : <?php
+                    <p>Date : <?php echo $annee; ?></p>
+                    <p>Technologies :</p>
+                        <ul class="technologies-logos">
+                        <?php
                         if (!empty($technologies) && !is_wp_error($technologies)) {
-                            $technologies_list = wp_list_pluck($technologies, 'name'); 
-                            echo esc_html(implode(', ', $technologies_list)); 
-                        } ?>
-                    </p> 
-                </div>
-                <div class="photo-content">
-                <?php 
-                    $content = get_the_content();
-                    $content_without_images = preg_replace('/<img[^>]*>/', '', $content);
-                    echo apply_filters('the_content', $content_without_images); 
-                ?>
+                            foreach ($technologies as $technology) {
+                                // Récupérer le logo via ACF
+                                $logo_url = get_field('logo', $technology);
+
+                                // Affichage du logo ou du nom si pas de logo
+                                if ($logo_url) {
+                                    echo '<li>';
+                                    echo '<img src="' . esc_url($logo_url) . '" alt="' . esc_attr($technology->name) . '" class="logo-image">';
+                                    echo '</li>';
+                                } else {
+                                    echo '<li>' . esc_html($technology->name) . '</li>';
+                                }
+                            }
+                        }
+                        ?>
+                        </ul>
+
                 </div>
             </div>
 
-            <div class="photo">
-                <?php 
-                // Extraire uniquement les images insérées dans the_content()
-                $content = get_the_content();
-                preg_match_all('/<img[^>]+>/i', $content, $matches); 
-                foreach ($matches[0] as $image) {
-                    echo $image; // Afficher chaque image séparément
-                }
-                ?>
+            <div class="photo-and-content">
+                <div class="photo">
+                    <?php 
+                    // Extraire uniquement les images insérées dans the_content()
+                    $content = get_the_content();
+                    preg_match_all('/<img[^>]+>/i', $content, $matches); 
+                    foreach ($matches[0] as $image) {
+                        echo $image; // Afficher chaque image séparément
+                    }
+                    ?>
+                </div>
+
+                <div class="photo-content">
+                    <?php 
+                        $content = get_the_content();
+                        $content_without_images = preg_replace('/<img[^>]*>/', '', $content);
+                        echo apply_filters('the_content', $content_without_images); 
+                    ?>
+                </div>
             </div>
         </figure>
-
-
+        
     <div>
         <div class="separator"></div>
     </div>
