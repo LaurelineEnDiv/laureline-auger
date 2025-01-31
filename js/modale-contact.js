7e36c1d8f17969cb9d90f0ee28726189bf679076
Laureline Auger <laureline.auger@gmail.com>
2025-01-31 10:35:41 +0100

    stylisation formulaire

--- js/modale-contact.js
+++ js/modale-contact.js
@@ -2,29 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const contactLinks = document.querySelectorAll('.open-modale'); 
     const modal = document.getElementById("contact-modal");
     const overlay = document.getElementById("modal-overlay");
-    const photoReferenceField = document.getElementById("photo-reference-field"); // Champ caché
-
-    // Réinitialiser le champ de référence photo
-    const resetPhotoReferenceField = () => {
-        if (photoReferenceField) {
-            photoReferenceField.value = ""; //Vider le champ à chaque ouverture
-        }
-    };
     
     // Ouvrir la modale au clic sur le lien Contact avec classe open-modale
     contactLinks.forEach(contact => {
         contact.addEventListener("click", (event) => {
             event.preventDefault();
-
-        //Ouverture de la modale avec champ Référence prérempli
-            // Récupérer la référence de l'attribut data-ref
-            const ref = contact.getAttribute("data-ref");
-            // Si l'élément avec l'ID photo-reference-field existe (le champ de formulaire caché) et si une valeur ref a été récupérée
-            if (photoReferenceField && ref) {
-                photoReferenceField.value = ref; // Préremplir le champ avec la référence
-            } else {
-                resetPhotoReferenceField(); // Réinitialiser si aucune référence n'est définie
-            }
 
             // Ajout de la classe "active"
             modal.classList.add("active");
@@ -37,7 +19,10 @@ document.addEventListener("DOMContentLoaded", function() {
     overlay.addEventListener("click", () => {
         modal.classList.remove("active");
         overlay.classList.remove("active");
-        document.body.style.overflow = ''; // Réactive le défilement
+
+        setTimeout(() => {
+            document.body.style.overflow = ''; // Réactive le défilement
+        }, 500); // Durée synchronisée avec la transition CSS
     });  
 });
 
--- template_parts/contact.php
+++ template_parts/contact.php
@@ -1,5 +1,6 @@
 <div class="modal-overlay" id="modal-overlay"></div>
 <div id="contact-modal">
+        <h1>Un petit mot ?</h1>
         <div class="contact-form">
             <?php
             echo do_shortcode('[contact-form-7 id="b6cfdab" title="Formulaire de contact"]');
