20898183ebf096cdce6d6499be445cc3f6c3ba6c
Laureline Auger <laureline.auger@gmail.com>
2025-02-18 21:51:22 +0100

    sous-menu services sur mobile

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const burgerMenu = document.querySelector('.burger-menu');
     const closeMenu = document.querySelector('.close-menu');
     const mobileMenu = document.querySelector('#mobile-menu-container');
-    const menuLinks = document.querySelectorAll('.mobile-nav .mobile-menu a'); // Sélecteur des liens de menu
+    const menuLinks = document.querySelectorAll('.mobile-nav .mobile-menu a'); // Liens du menu
+    const menuItemsWithSubMenu = document.querySelectorAll('.mobile-menu li:has(.sub-menu)'); // Items de menu avec sous-menu
 
     // Ouvrir le menu
     burgerMenu.addEventListener('click', () => {
@@ -20,6 +21,19 @@ document.addEventListener('DOMContentLoaded', () => {
             // Vérifie si l'URL du lien contient une ancre
             if (link.hash) {
                 mobileMenu.classList.remove('active'); // Ferme le menu
+            }
+        });
+    });
+
+    // Afficher/masquer le sous-menu lors du clic sur un élément de menu mobile
+    menuItemsWithSubMenu.forEach(item => {
+        item.addEventListener('click', (e) => {
+            // Empêcher la propagation du clic pour éviter la fermeture immédiate du menu
+            e.stopPropagation();
+
+            const subMenu = item.querySelector('.sub-menu');
+            if (subMenu) {
+                subMenu.classList.toggle('active'); // Toggle la classe 'active' pour afficher/masquer le sous-menu
             }
         });
     });
