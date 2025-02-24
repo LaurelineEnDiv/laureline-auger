02f478576eacce9b39041840ac1b8f386849437b
Laureline Auger <laureline.auger@gmail.com>
2025-02-24 14:50:02 +0100

    home-page : ajout section services maintenace

--- index.php
+++ index.php
@@ -27,15 +27,15 @@
         </section>
 
         <section class="services">
-            <h2>Optimisation SEO <br>pour un meilleur référencement naturel</h2>
+        <h2>Optimisation SEO <br><span class="subtitle">pour un meilleur référencement naturel</span></h2>
             <div class="seo-content">
                 <div class="seo-text">
                     <p>Un site rapide, structuré et optimisé pour les moteurs de recherche est essentiel pour améliorer votre visibilité en ligne. 
                         En tant qu'experte SEO, je vous propose d'améliorer votre référencement naturel selon trois fondamentaux :</p>
                     <ul>
-                        <li><div class="top-line"><i class="fas fa-chart-line"></i><strong>SEO technique :</strong></div><span class="subtitle">Amélioration du code, de la vitesse et de la structure.</span></li>
+                        <li><div class="top-line"><i class="fa-solid fa-code"></i><strong>SEO technique :</strong></div><span class="subtitle">Amélioration du code, de la vitesse et de la structure.</span></li>
                         <li><div class="top-line"><i class="fas fa-search"></i><strong>SEO sémantique :</strong></div><span class="subtitle">Contenu optimisé pour le référencement naturel.</span></li>
-                        <li><div class="top-line"><i class="fas fa-bullseye"></i><strong>Expérience utilisateur :</strong></div><span class="subtitle">Navigation fluide et performance optimisée.</span></li>
+                        <li><div class="top-line"><i class="fa-solid fa-eye"></i><strong>Expérience utilisateur :</strong></div><span class="subtitle">Navigation fluide et performance optimisée.</span></li>
                     </ul>
                 </div>
                 <div class="seo-image">
@@ -43,6 +43,25 @@
                 </div>
             </div>
             <a href="https://laureline-auger.fr/optimisation-seo/" class="cta-button">Je veux optimiser mon site pour le référencement naturel</a>
+        </section>
+
+        <section class="services">
+            <h2>Support & Maintenance <br><span class="subtitle">pour un site web sécurisé et performant</span></h2>
+            <div class="support-content">
+                <div class="support-image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/maintenance-site-web.jpg" alt="Support et Maintenance illustration">
+                </div>
+                <div class="support-text">
+                    <p>Un site Wordpress nécessite un suivi régulier pour garantir sa sécurité, ses performances et son bon fonctionnement. 
+                    Je propose des solutions de maintenance adaptées à vos besoins :</p>
+                    <ul>
+                    <li><div class="top-line"><i class="fas fa-shield-alt"></i><strong>Sécurité :</strong></div><span class="subtitle">Mises à jour régulières et protection contre les attaques.</span></li>
+                    <li><div class="top-line"><i class="fas fa-tachometer-alt"></i><strong>Performances :</strong></div><span class="subtitle">Optimisation continue de la vitesse et des ressources.</span></li>
+                    <li><div class="top-line"><i class="fas fa-life-ring"></i><strong>Assistance :</strong></div><span class="subtitle">Support technique et corrections rapides en cas de problème.</span></li>
+                    </ul>
+                </div>
+            </div>
+             <a href="https://laureline-auger.fr/support-maintenance/" class="cta-button">J'ai besoin d'un support technique</a>
         </section>
 
     <!--------- Liste des projets ------------>
